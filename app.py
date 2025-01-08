e9c54ac32567e2266d34fe97743eb73682bd76f1
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2025-01-08 23:10:27 +0530

    Update app.py change the line spaces of prompt message

--- app.py
+++ app.py
@@ -101,9 +101,12 @@ with tab1:
         if text:
             vector_store = create_vector_store(text)
             prompt_message = (
-                """
-                Imagine you are a Senior Business Analyst. Your responsibility is to read the entire Business Requirement Document (BRD) and convert it into detailed User Stories. Think step-by-step and ensure you write all possible User Stories derived from the BRD. Provide fully complete User Stories only, without any additional explanation or sentences.
-                """
+                "Imagine you are a Senior Business Analyst. "
+                "Your responsibility is to read the entire Business Requirement Document (BRD) "
+                "and convert it into detailed User Stories. "
+                "Think step-by-step and ensure you write all possible User Stories derived from the BRD." 
+                "Provide fully complete User Stories only, "
+                "without any additional explanation or sentences."
             )
             start_query_time = time.time()
             matches = vector_store.similarity_search(prompt_message, k=3)
